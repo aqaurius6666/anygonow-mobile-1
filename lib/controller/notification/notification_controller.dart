c4004690d78dabe47485e6d133e7db8d5da09b25
nam.nlh <nam.nlh@gmail.com>
2022-11-27 20:39:39 +0700

    fix: phone input

--- lib/controller/notification/notification_controller.dart
+++ lib/controller/notification/notification_controller.dart
@@ -1,8 +1,10 @@
-import 'dart:convert';
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
+import 'package:untitled/controller/brand_detail/brand_detail_controller.dart';
 import 'package:untitled/controller/global_controller.dart';
+import 'package:untitled/screen/brand_detail/brand_detail.dart';
+import 'package:untitled/screen/chat/chat_screen.dart';
+import 'package:untitled/screen/handyman/home_page/home_page_screen.dart';
 import 'package:untitled/service/local_notification_service.dart';
 
 class NotificationController extends GetxController {
@@ -13,25 +15,69 @@ class NotificationController extends GetxController {
     // TODO: implement onInit
     //background but kill
     FirebaseMessaging.instance.getInitialMessage().then((message) async {
-      if (message != null) {
-        print("bam vafo noti");
-      }
+      if (message == null) return;
+      print("Noti background but kill");
+      await handleNotification(message.data);
     });
 
     //foreground
     FirebaseMessaging.onMessage.listen((message) {
-      print(message.notification!.body);
-      print(message.notification!.title);
+      print("Noti foreground");
+
       LocalNotificationService.display(message);
     });
 
     //background but opened
     FirebaseMessaging.onMessageOpenedApp.listen((message) async {
-      print(message.notification!.body);
-      print(message.notification!.title);
-      // await notiAction(message.data["id"]);
+      print("Noti background but opened");
+      await handleNotification(message.data);
     });
 
     super.onInit();
+  }
+
+  Future handleNotification(dynamic data) async {
+    String notiType = data["type"];
+    switch (notiType) {
+      case "request-notification":
+        Get.back();
+        Get.to(() => HandymanHomePageScreen());
+        break;
+      case "cancel-notification":
+        Get.back();
+        Get.to(() => HandymanHomePageScreen());
+        break;
+      case "complete-notification":
+        String id = data["businessId"];
+        var brandDetailController = Get.put(BrandDetailController());
+        var res = await brandDetailController.getBusinessDetail(id: id);
+        var serviceRes =
+            await brandDetailController.getBusinessServices(id: id);
+        var ratingRes = await brandDetailController.getBusinessRating(id: id);
+        await brandDetailController.getBusinessFeedback(id: id);
+        if (res != null && serviceRes && ratingRes) {
+          Get.back();
+          Get.to(() => BrandDetailScreen());
+        }
+        break;
+      case "connect-notification":
+        String id = data["conversationId"];
+        Get.back();
+        Get.to(() => ChatScreen());
+        break;
+      case "reject-notification":
+        String id = data["businessId"];
+        var brandDetailController = Get.put(BrandDetailController());
+        var res = await brandDetailController.getBusinessDetail(id: id);
+        var serviceRes =
+            await brandDetailController.getBusinessServices(id: id);
+        var ratingRes = await brandDetailController.getBusinessRating(id: id);
+        await brandDetailController.getBusinessFeedback(id: id);
+        if (res != null && serviceRes && ratingRes) {
+          Get.back();
+          Get.to(() => BrandDetailScreen());
+        }
+        break;
+    }
   }
 }
